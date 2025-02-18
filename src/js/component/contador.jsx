9411a92b49b81a28c2bf906931b8ee7f5e4c462b
alex15328 <alexmen15328@gmail.com>
2025-02-18 20:08:47 +0000

    contador simple1

---
+++ src/js/component/contador.jsx
@@ -0,0 +1,3 @@
+import React from "react";
+export const Contador = ({number}) => <div className="contador">{number}</div>
+    
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,119 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { Contador } from "./contador.jsx";
+
 
 //create your first component
 const Home = () => {
+
+	const [timer, setTimer] = useState(0)
+	const [active, setActive] = useState(false)
+	const [isCountdown, setIsCountdown] = useState(false)
+	const [alert, setAlert] = useState (0)
+
+
+
+	useEffect(() => {		
+		if(timer == alert && alert != 0) window.alert("time's up!")
+
+		if (active) {
+			setTimeout(() => {
+				setTimer(value => value + 1)
+			}, 1000)
+		}
+		if (isCountdown) {
+			setTimeout(() => {
+				setTimer(value => value - 1)
+			}, 1000)
+		}
+
+	}, [timer, active, isCountdown])
+
+	const startStop = () => setActive(value => !value)
+	const resetTimer = () => setTimer(value=> value=0 )
+	const handleChange = e => setTimer(value=> value= e.target.value)
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<main className="text-center">
+			<section className="counter-holder">
+
+				<Contador number={<span className="fa-regular fa-clock"></span>} />
+				<Contador number={Math.floor(timer / 100000) % 10} />
+				<Contador number={Math.floor(timer / 10000) % 10} />
+				<Contador number={Math.floor(timer / 1000) % 10} />
+				<Contador number={Math.floor(timer / 100) % 10} />
+				<Contador number={Math.floor(timer / 10) % 10} />
+				<Contador number={Math.floor(timer % 10)} />
+			</section>
+			<section className="container text-center my-5">
+				<h2>Contador</h2>
+				<div>
+					<button
+						disabled={active}
+						onClick={startStop} className="mx-1 btn bg-primary">Start</button>
+					<button
+						disabled={!active}
+						onClick={startStop} className="mx-1 btn btn-danger">Stop</button>
+					<button onClick={resetTimer} className="mx-1 btn bg-warning">Reset</button>
+				</div>
+			</section>
+			<section className="container text-center">
+				<h2>Cuenta atras</h2>
+				<form
+					className="form-control"
+					onSubmit={e=>e.preventDefault()}>
+					<label className="form-text">
+						Empezar:
+						<input
+							className="form-control"
+							type="number"
+							value={timer}
+							onChange={e=> handleChange(e)}
+						/>
+					</label>
+					<div>
+
+						<input
+							disabled={isCountdown}
+							onClick={()=>setIsCountdown(value => !value)}
+							className="my-3 mx-1 btn bg-primary" type="submit" value={"start"}/>
+						<input
+							disabled={!isCountdown}
+							onClick={()=>setIsCountdown(value => !value)}
+							className="my-3 mx-1 btn btn-danger" type="submit" value={"stop"} />
+					</div>
+				</form>
+			</section>
+			<section className="container text-center my-5">
+				<h2>Alerta</h2>
+
+
+				<form
+					className="form-control"
+					onSubmit={e => e.preventDefault()}>
+					<label className="form-text">
+						Alerta en:
+						<input
+							className="form-control"
+							type="number"							
+							onChange={e => setAlert(value => value = e.target.value)}
+						/>
+					</label>
+					<div>
+
+						<input							
+							onClick={()=>window.alert("Alert created")}
+							className="my-3 m-1 btn bg-info" type="submit" value={"Crear"} />
+					</div>
+				</form>
+			</section>
+		</main >
 	);
 };
 
 export default Home;
+
+
